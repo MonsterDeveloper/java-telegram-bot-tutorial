a0642f89dfba6c68ec3caac275e3118835784bc7
Andrew <slider7259@gmail.com>
2017-02-07 11:35:16 +0300

    Create Main.java

---
+++ Lesson 2/src/Main.java
@@ -0,0 +1,19 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            botsApi.registerBot(new PhotoBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+        System.out.println("PhotoBot successfully started!");
+    }
+}
