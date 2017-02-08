dfecd0d4bd51b904674e6096d09981ca62ef496f
Andrew <slider7259@gmail.com>
2017-02-08 16:24:20 +0300

    Added source code to Lesson 6

---
+++ Lesson 6/src/Main.java
@@ -0,0 +1,20 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Main {
+    public static void main(String[] args) {
+        // Initialize Api Context
+        ApiContextInitializer.init();
+
+        // Instantiate Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        // Register our bot
+        try {
+            botsApi.registerBot(new BotApi20());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
