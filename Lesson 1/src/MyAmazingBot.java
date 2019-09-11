b13cb0f7d48d2562a38a897b23d201081228ce4a
2Fey <54020692+2Fey@users.noreply.github.com>
2019-09-11 16:26:22 +0300

    Update MyAmazingBot.java
    api update

--- Lesson 1/src/MyAmazingBot.java
+++ Lesson 1/src/MyAmazingBot.java
@@ -1,7 +1,7 @@
-import org.telegram.telegrambots.api.methods.send.SendMessage;
-import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class MyAmazingBot extends TelegramLongPollingBot {
     @Override
